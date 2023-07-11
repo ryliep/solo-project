e034617775d29918b67a8c9f6af784c23f4a1493
Rylie <ryles@MacBook-Pro-4.ht.home>
2023-07-10 19:47:05 -0700

    more boilerplate details. see screenshots and prev unit for webpack and package.json set up

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, createRoot } from "react-dom";
-import App from "./App";
+import { render, createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
